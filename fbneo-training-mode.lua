7af0736796a13537d474c8e6ebcc76785cab1c03
gigasturtz <gigasturtz@gmail.com>
2021-03-17 19:57:39 -0400

    Adds a comma :)

--- fbneo-training-mode.lua
+++ fbneo-training-mode.lua
@@ -18,7 +18,7 @@ local games = {
 	lb2 = {"lastbld2", hitboxes = "cps3-hitboxes", iconfile = "icons-neogeo-32.png"},
 	samsho3 = {"samsho3", iconfile = "icons-neogeo-32.png"},
 	ssf2xjr1 = {"ssf2xjr1", hitboxes = "sf2-hitboxes", iconfile = "icons-capcom-32.png"},
-	vhuntjr2 = {"vhuntjr2", hitboxes = "cps2-hitboxes", iconfile = "icons-capcom-32.png"}
+	vhuntjr2 = {"vhuntjr2", hitboxes = "cps2-hitboxes", iconfile = "icons-capcom-32.png"},
 	garou = {"garou", hitboxes = "garou-hitboxes", iconfile = "icons-neogeo-32.png"},
 }
 
