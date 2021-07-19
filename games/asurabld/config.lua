94303df0b7632824bcab971e88eac1f954be2f41
Bee-Chan <86084423+Bee-Chan@users.noreply.github.com>
2021-07-19 16:45:36 -0500

    Support for Asura Blade

---
+++ games/asurabld/config.lua
@@ -0,0 +1,103 @@
+assert(rb,"Run fbneo-training-mode.lua")
+return {
+-- Table: {1}
+{
+   ["recording"]={2},
+   ["hud"]={3},
+   ["changed"]=false,
+   ["hitbox"]={4},
+   ["p2"]={5},
+   ["interactivegui"]={6},
+   ["inputs"]={7},
+   ["p1"]={8},
+},
+-- Table: {2}
+{
+   ["skiptostart"]=false,
+   ["skiptofinish"]=false,
+},
+-- Table: {3}
+{
+   ["p1meterenabled"]=true,
+   ["p2healthtextcolour"]=4294967295,
+   ["p1healthtextcolour"]=4294967295,
+   ["p1metertextcolour"]=4294967295,
+   ["p2metertextcolour"]=4294967295,
+   ["p2meterenabled"]=true,
+   ["comboenabled"]=true,
+   ["p1meterx"]=111,
+   ["p2meterx"]=202,
+   ["combotextcolour"]=4294902015,
+   ["p2healthy"]=16,
+   ["p1healthenabled"]=true,
+   ["totaltextcolour"]=16711935,
+   ["p1metery"]=226,
+   ["combotexty"]=43,
+   ["damagetextcolour"]=16711935,
+   ["combotextcolour2"]=16711935,
+   ["p2metery"]=226,
+   ["p2healthenabled"]=true,
+   ["combotextx"]=143,
+   ["p1healthx"]=24,
+   ["p2healthx"]=285,
+   ["p1healthy"]=16,
+},
+-- Table: {4}
+{
+   ["enabled"]=true,
+},
+-- Table: {5}
+{
+   ["instantrefillmeter"]=true,
+   ["refillmeterenabled"]=true,
+   ["refillmeterspeed"]=10,
+   ["refillhealthspeed"]=10,
+   ["instantrefillhealth"]=false,
+   ["refillhealthenabled"]=true,
+},
+-- Table: {6}
+{
+   ["boxxd"]=8,
+   ["boxym"]=9,
+   ["barcolour"]="yellow",
+   ["boxyd"]=10,
+   ["ol"]=255,
+   ["selectioncolour"]=4278190335,
+   ["bg"]=4042322175,
+   ["boxxm"]=7,
+},
+-- Table: {7}
+{
+   ["state"]={9},
+   ["simpleinputenabled"]=true,
+   ["iconsize"]=10,
+   ["coinleniency"]=10,
+   ["scrollinginputxoffset"]={10},
+   ["scrollinginputyoffset"]={11},
+   ["framenumbersenabled"]=false,
+},
+-- Table: {8}
+{
+   ["instantrefillmeter"]=true,
+   ["refillmeterenabled"]=true,
+   ["refillmeterspeed"]=10,
+   ["refillhealthspeed"]=10,
+   ["instantrefillhealth"]=true,
+   ["refillhealthenabled"]=true,
+},
+-- Table: {9}
+{
+   true,
+   true,
+},
+-- Table: {10}
+{
+   10,
+   290,
+},
+-- Table: {11}
+{
+   60,
+   60,
+},
+}
